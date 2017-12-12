38edbe868d8a3c07a15059b609fa1da3564c2c46
Wil Donovan <wil.donovan@gmail.com>
2017-12-12 12:14:49 -0700

    hurry mimi

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('./knexfile')[environment]
 const database = require('knex')(configuration)
 const path = require("path");
+const Foods = require('./lib/models/foods')
 
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Quantified Self API'
@@ -14,6 +15,34 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname+'/welcome.html'))
+})
+
+app.get('/api/v1/foods', (request, response) => {
+  Foods.allFoods()
+  .then((data) => {
+    if (data.rowCount == 0) {return response.sendStatus(404)}
+    response.json(data.rows)
+  })
+})
+
+app.post('/api/v1/foods', (request, response) => {
+  const food = request.body;
+  if (food.name == '' || food.calories == '') {
+    return response.status(422).send({ error: "Make sure all properties are provided!"})
+  }
+
+  Foods.addFood(food).then((data) => {
+    food.id = data.rows[0].id
+    response.status(201).json(food)
+  })
+})
+
+app.delete('/api/v1/foods/:id', function(request, response){
+  const id = request.params.id
+
+  Foods.deleteFood(id).then((data) => {
+    response.status(201).json(id)
+  })
 })
 
 if(!module.parent) {
--- test/server-test.js
+++ test/server-test.js
@@ -4,6 +4,7 @@ const request = require('request')
 const environment = process.env.NODE_ENV || 'test'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
+const Foods = require('../lib/models/foods')
 
 describe('Server', function(){
   before(function(done){
@@ -24,3 +25,162 @@ describe('Server', function(){
   it('should exist', function(){
     assert(app)
   })
+
+  describe('GET /', function(){
+    it('should return a 200', function(done){
+      this.request.get('/', function(error, response){
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 200)
+        done()
+      })
+    })
+
+    it('should have a body with the name of the application', function(done){
+      this.request.get('/', function(error, response){
+        if (error) { done(error) }
+        assert.include(response.body, app.locals.title)
+        done()
+      })
+    })
+  })
+
+  describe('GET /api/foods/', function(){
+    beforeEach((done) => {
+      Promise.all([
+        Foods.createFood(["Hamburger", 150, new Date, new Date]),
+        Foods.createFood(["Pizza", 200, new Date, new Date]),
+        Foods.createFood(["Pasta", 75, new Date, new Date]),
+      ]).then((data) => done())
+    })
+
+    afterEach(function(done) {
+      Foods.destroyAll()
+        .then(() => done())
+    })
+
+    it('should return a 404 if the resource is not found', function(done){
+      this.request.get('/api/foods/1000', function(error, response){
+        if (error) { done(error) }
+        assert.equal(response.statusCode, 404)
+        done()
+      })
+    })
+
+    it('should have all foods', function(done){
+      this.request.get(('/api/v1/foods/' ), function(error, response){
+
+        if (error) { done(error) }
+
+        let parsedSecret = JSON.parse(response.body)
+
+        assert.equal(parsedSecret.length, 3)
+        done()
+      })
+    })
+  })
+  // describe('GET /api/foods/:id', function(){
+  //   beforeEach((done) => {
+  //     Promise.all([
+  //       Foods.createFood(["Hamburger", 150, new Date, new Date]),
+  //       Foods.createFood(["Pizza", 200, new Date, new Date]),
+  //       Foods.createFood(["Pasta", 75, new Date, new Date]),
+  //     ]).then((data) => done())
+  //   })
+  //
+  //   afterEach(function(done) {
+  //     Foods.destroyAll()
+  //       .then(() => done())
+  //   })
+  //
+  //   it('should return a 404 if the resource is not found', function(done){
+  //     this.request.get('/api/foods/1000', function(error, response){
+  //       if (error) { done(error) }
+  //       assert.equal(response.statusCode, 404)
+  //       done()
+  //     })
+  //   })
+  //
+  //   it('should have single food attrs', function(done){
+  //     var id = 1
+  //     var name = "Hamburger"
+  //     var calories = 150
+  //     this.request.get(('/api/v1/foods/' + id ), function(error, response){
+  //
+  //       if (error) { done(error) }
+  //
+  //       let parsedSecret = JSON.parse(response.body)
+  //
+  //       assert.equal(parsedSecret.id, id)
+  //       assert.equal(parsedSecret.name, name)
+  //       assert.equal(parsedSecret.calories, calories)
+  //       done()
+  //     })
+  //   })
+  // })
+  //
+  // describe('POST /api/foods', function(){
+  //
+  //   it('should receive and store data', function(done){
+  //     var name = "Hamburger"
+  //     var calories = 150
+  //     var created_at = new Date
+  //     var updated_at = new Date
+  //     var food = {name, calories, created_at, updated_at}
+  //
+  //     this.request.post('/api/v1/foods', { form: food }, function(error, response){
+  //       if (error) { done(error) }
+  //       assert.equal(response.statusCode, 201);
+  //       done()
+  //     })
+  //   })
+  // })
+  // describe('Patch /api/foods/:id', function(){
+  //   beforeEach((done) => {
+  //     Promise.all([
+  //       Foods.createFood(["Hamburger", 150, new Date, new Date]),
+  //       Foods.createFood(["Pizza", 200, new Date, new Date]),
+  //       Foods.createFood(["Pasta", 75, new Date, new Date]),
+  //     ]).then((data) => done())
+  //   })
+  //
+  //   afterEach(function(done) {
+  //     Foods.destroyAll()
+  //       .then(() => done())
+  //   })
+  //
+  //   it('should update receive and store updated data', function(done){
+  //     var name = "Buffalo Burger"
+  //     var calories = 175
+  //     var food = {name, calories}
+  //
+  //     this.request.patch('/api/v1/foods/1', { form: food }, function(error, response){
+  //       if (error) { done(error) }
+  //       assert.equal(response.statusCode, 201);
+  //       done()
+  //     })
+  //   })
+  // })
+  //
+  // describe('Delete /api/foods/:id', function(){
+  //   beforeEach((done) => {
+  //     Promise.all([
+  //       Foods.createFood(["Hamburger", 150, new Date, new Date]),
+  //       Foods.createFood(["Pizza", 200, new Date, new Date]),
+  //       Foods.createFood(["Pasta", 75, new Date, new Date]),
+  //     ]).then((data) => done())
+  //   })
+  //
+  //   afterEach(function(done) {
+  //     Foods.destroyAll()
+  //       .then(() => done())
+  //   })
+  //
+  //   it('should delete a food data', function(done){
+  //     this.request.delete('/api/v1/foods/1', function(error, response){
+  //       if (error) { done(error) }
+  //       assert.equal(response.statusCode, 201);
+  //       done()
+  //     })
+  //   })
+  // })
+})
