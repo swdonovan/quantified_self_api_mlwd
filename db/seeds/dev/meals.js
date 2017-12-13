a13c8ee6f9415482feb6daf8b55530bb6fbca491
Wil Donovan <wil.donovan@gmail.com>
2017-12-13 10:12:07 -0700

    fixed seed file to match the client side configurations

--- db/seeds/dev/meals.js
+++ db/seeds/dev/meals.js
@@ -1,32 +1,12 @@
 
-
-const meals = [
-  "Breakfast",
-  "Snack",
-  "Lunch",
-  "Dinner"
-]
-
 exports.seed = function(knex, Promise) {
   return knex.raw('TRUNCATE meals RESTART IDENTITY')
   .then(function () {
-    return Promise.all([
-      knex.raw(
-        'INSERT INTO meals (name, created_at, updated_at) VALUES (?,?,?)',
-        ["Breakfast", new Date, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO meals (name, created_at, updated_at) VALUES (?,?,?)',
-        ["Snack", new Date, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO meals (name, created_at, updated_at) VALUES (?,?,?)',
-        ["Lunch", new Date, new Date]
-      ),
-      knex.raw(
-        'INSERT INTO meals (name, created_at, updated_at) VALUES (?,?,?)',
-        ["Dinner", new Date, new Date]
-      )
+    return knex('meals').insert([
+      {name: 'Breakfast', created_at: new Date, updated_at: new Date},
+      {name: 'Sanck', created_at: new Date, updated_at: new Date},
+      {name: 'Lunch', created_at: new Date, updated_at: new Date},
+      {name: 'Dinner', created_at: new Date, updated_at: new Date}
     ])
   })
 }
