4e340777619da27ee09342d4a9fc6ddb6a603466
Wil Donovan <wil.donovan@gmail.com>
2017-12-13 14:49:34 -0700

    more cors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const Meals = require('./lib/models/meals')
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next()
 })
