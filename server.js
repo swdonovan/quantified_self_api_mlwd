5edc5ae9dc5f9c3883d69b3e1b9072005f69535e
Wil Donovan <wil.donovan@gmail.com>
2017-12-13 14:47:22 -0700

    cors

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+  next()
+})
 app.set('port', process.env.PORT || 3000)
 app.locals.title = 'Quantified Self API'
 
